33767c96aba7be3c4ea5eebe3ae58dbfb76bcd78
DarshanSY <darshansy318@gmail.com>
2026-07-04 00:15:16 +0530

    Flatten repository structure to root folder

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
 echo Starting ATM Management System...
-cd ATM-Management-System-main
 java -cp "bin;C:\Users\darsh\.m2\repository\com\mysql\mysql-connector-j\8.0.33\mysql-connector-j-8.0.33.jar" com.atm.gui.LoginFrame
 pause
